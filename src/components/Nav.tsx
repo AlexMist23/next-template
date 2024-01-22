608d891e62299e189510d44425d8ac60fca9f0fa
Alex <aleksander.misterkiewicz@outlook.com>
2024-01-22 13:11:12 +0100

    nav bar p.1

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,18 +7,45 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@radix-ui/react-navigation-menu";
+import { usePathname } from "next/navigation";
+
+import { ModeToggle } from "./themeToggleButton";
+import Logo from "@/icons/logo";
+import Link from "next/link";
 
 export default function Nav() {
+  const pathname = usePathname();
+  const navItems = [
+    { path: "/", label: "Home" },
+    { path: "/about", label: "About" },
+    { path: "/contact", label: "Contact" },
+  ];
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <header className="container flex h-12 max-w-screen-2xl items-center border-b-2 border-muted">
+      <div className="hidden md:flex justify-start gap-6">
+        <div className="flex gap-2">
+          <Logo className="h-6 w-6 fill-foreground" />
+          <p className="font-semibold">NexTemplate</p>
+        </div>
+        <ul className="flex gap-4">
+          {navItems.map((item) => (
+            <Link
+              key={item.label}
+              href={item.path}
+              className={`hover:text-foreground ${
+                pathname === item.path
+                  ? "text-accent-foreground"
+                  : "text-muted-foreground"
+              } `}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+        <ModeToggle />
+      </div>
+    </header>
   );
 }
--- src/icons/logo.tsx
+++ src/icons/logo.tsx
@@ -1,13 +1,13 @@
-export default function Logo() {
+export default function Logo(props: { className: string }) {
   return (
     <svg
+      className={props.className}
       width="76"
       height="65"
       viewBox="0 0 76 65"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="#ffffff" />
+      <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" />
     </svg>
   );
 }
