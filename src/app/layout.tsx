e9d1009a55712cba41489b0e8f445f62a6d10af3
Alex <aleksander.misterkiewicz@outlook.com>
2024-02-03 10:57:18 +0100

    1234

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import Nav from "@/components/Nav";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
