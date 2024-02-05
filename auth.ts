9473f649a1aa9691acc9913c08003146d06ddb30
Alex <aleksander.misterkiewicz@outlook.com>
2024-02-05 19:40:00 +0100

    12

--- auth.ts
+++ auth.ts
@@ -1,26 +1,3 @@
 import NextAuth from "next-auth";
-
 import GitHub from "next-auth/providers/github";
-// import Google from "next-auth/providers/google"
-// import Spotify from "next-auth/providers/spotify"
-
-import type { NextAuthConfig } from "next-auth";
-
-export const config = {
-  theme: {
-    logo: "https://next-auth.js.org/img/logo/logo-sm.png",
-  },
-  providers: [
-    GitHub,
-    // Google,
-  ],
-  callbacks: {
-    authorized({ request, auth }) {
-      const { pathname } = request.nextUrl;
-      if (pathname === "/middleware-example") return !!auth;
-      return true;
-    },
-  },
-} satisfies NextAuthConfig;
-
-export const { handlers, auth, signIn, signOut } = NextAuth(config);
+export const { handlers, auth } = NextAuth({ providers: [GitHub] });
