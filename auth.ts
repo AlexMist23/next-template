a8669c6404cdedfd9bfffee42dc0b401f6b6570b
Alex <aleksander.misterkiewicz@outlook.com>
2024-02-05 22:24:06 +0100

    connection to the database

--- auth.ts
+++ auth.ts
@@ -1,3 +1,29 @@
+import process from "process";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-export const { handlers, auth } = NextAuth({ providers: [GitHub] });
+import Email from "next-auth/providers/email";
+import PostgresAdapter from "@auth/pg-adapter";
+
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
+
+export const { handlers, auth } = NextAuth({
+  adapter: PostgresAdapter(pool),
+  providers: [
+    GitHub,
+    Email({
+      server: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+});
