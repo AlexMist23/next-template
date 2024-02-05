e76808f557d03d06f326fc6a4098baad006114ab
Alex <aleksander.misterkiewicz@outlook.com>
2024-02-05 23:12:43 +0100

    add creditentials signing

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import process from "process";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Email from "next-auth/providers/email";
+import CredentialsProvider from "next-auth/providers/credentials";
 import PostgresAdapter from "@auth/pg-adapter";
 
 import { Pool } from "pg";
@@ -12,7 +13,6 @@ const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DATABASE,
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-  
 });
 
 export const { handlers, auth } = NextAuth({
@@ -29,6 +29,32 @@ export const { handlers, auth } = NextAuth({
         },
       },
       from: process.env.EMAIL_FROM,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
     }),
   ],
 });
