10383121c8e929c49c7182f9bac91d676ba4c794
Alex <aleksander.misterkiewicz@outlook.com>
2024-02-24 23:58:59 +0100

    relocate and insert dynamic svg's

---
+++ src/components/icons/vercel-logo-icon.tsx
@@ -0,0 +1,22 @@
+interface VercelLogoProps {
+  className?: string
+}
+
+export default function VercelLogo({ className }: VercelLogoProps) {
+  return (
+    <svg
+      className={className}
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.49998 1L6.92321 2.00307L1.17498 12L0.599976 13H1.7535H13.2464H14.4L13.825 12L8.07674 2.00307L7.49998 1ZM7.49998 3.00613L2.3285 12H12.6714L7.49998 3.00613Z"
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  )
+}
