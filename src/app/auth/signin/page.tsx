19e5df424b92f3b3778e4647bc8b4f6e4b2418b3
Alex <aleksander.misterkiewicz@outlook.com>
2024-02-25 00:03:04 +0100

    del unused imports

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
@@ -6,12 +5,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { FormField, FormItem } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { signIn } from "@/lib/auth"
-import { auth0ProvidersList } from "@/lib/auth"
-import Auth0Forms from "./_components/auth0Forms"
-import EmailForm from "./_components/emailForm"
+import Auth0Form from "./_components/auth0-forms"
+import EmailForm from "./_components/email-form"
 export default async function Page() {
   return (
     <main className="flex justify-center container mx-auto px-4 py-12">
@@ -21,7 +16,7 @@ export default async function Page() {
           <CardDescription>Sign in to {"<AppName>"}</CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col space-y-1">
-          <Auth0Forms />
+          <Auth0Form />
           <EmailForm />
         </CardContent>
       </Card>
